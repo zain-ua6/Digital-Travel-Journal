656d12b4740b59b6e08c122d83008608d2d9d36c
zain-ua6 <zain.abideen6134@outlook.com>
2025-06-12 22:05:10 +0500

    Made changes in Trip.h

--- Trip.h
+++ Trip.h
@@ -3,6 +3,11 @@
 
 #include "String.h"
 #include "dynamic.h"
+#include "CheckList.h"
+#include "Note.h"
+#include "Expense.h"
+#include "Location.h"
+#include "TransportLeg.h"
 
 enum TripStatus{Completed, Current, Planned};
 
@@ -12,6 +17,21 @@ class Trip {
 	String endDate;
 	String travelBuddy;
 	TripStatus status;
+	CheckList checklist;
+	Dynamic_array<Note*> notes;
+	Dynamic_array<Expense*> expenses;
+	Dynamic_array<Location*> locations;
+	Dynamic_array<TransportLeg*> legs;
+private:
+	void addNote(Note*);
+	void addExpense(Expense*);
+	void addLocation(Location*);
+	void addTransportLeg(TransportLeg*);
+	void updateStatus(TripStatus);
+	void displayTripSummary();
+	void saveToFile(ofstream&);
+	void loadFromFile(ifstream&);
+
 };
 
 #endif
